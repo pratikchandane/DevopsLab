28117acf4ee7719fdca9bd0019241efe45d1e916
Pratik Chandane <155938540+pratikchandane@users.noreply.github.com>
2026-02-16 19:30:29 +0530

    Add files via upload

---
+++ jenkinsdemo.java
@@ -0,0 +1,17 @@
+public class JenkinsDemo {
+    public static void main(String[] args) {
+        
+        String projectName = "Java CI Project";
+        String status = "Build Completed Successfully";
+        
+        System.out.println("Welcome to " + projectName);
+        System.out.println("Jenkins Automation Status: " + status);
+        
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        
+        System.out.println("Sample Calculation Result: " + sum);
+        System.out.println("Jenkins is compiling and executing Java code automatically.");
+    }
+}
