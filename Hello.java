68c99648e6dd7cee775c26538a59b4e755416416
Pratik Chandane <155938540+pratikchandane@users.noreply.github.com>
2026-04-04 19:43:52 +0530

    updated java file for jenkins auto trigger

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins - updated!");
+        System.out.println("Hello Jenkins - Auto Trigger working!");
     }
 }
